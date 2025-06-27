0fdba3a5c67a241367c7f0077c84f2e117c89142
rahulkumardas45 <158608917+rahulkumardas45@users.noreply.github.com>
2025-06-27 14:18:48 +0530

    `Update dependencies and environment variables`

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
         
     } catch (error) {
-        console.log("Error connecting to MongoDB:", error);
+        console.log("MongoDB connection failled:", error);
         process.exit(1);
         
         
--- src/index.js
+++ src/index.js
@@ -4,14 +4,30 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
-
-
+import app from "./app.js";
 
 dotenv.config({ 
     path: "./.env"
- });
+ })
 
 connectDB()
+.then(() => {
+
+    app.on("error", (err)=>{
+        console.log("Server error", err);
+        throw  err
+    })
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+    console.log("MongoDB connected successfully");
+})
+.catch((error) => {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+})
+
 
 
 
